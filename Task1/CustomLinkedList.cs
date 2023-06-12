86e2ebf328d999fa90aeb204cb6412f60ebfae17
alex_di <mirag0505@yandex.ru>
2023-06-12 15:44:37 +0300

    Реализовал большую часть функционала

---
+++ Task1/CustomLinkedList.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using AlgorithmsDataStructures;
+
+namespace AlgorithmsDataStructures
+{
+
+    public class Node
+    {
+        public int value;
+        public Node next;
+        public Node(int _value) { value = _value; }
+    }
+
+    public class LinkedList
+    {
+        public Node head;
+        public Node tail;
+
+        public LinkedList()
+        {
+            head = null;
+            tail = null;
+        }
+
+        public void AddInTail(Node _item)
+        {
+            if (head == null) head = _item;
+            else tail.next = _item;
+            tail = _item;
+        }
+
+        public Node Find(int _value)
+        {
+            Node node = head;
+            while (node != null)
+            {
+                if (node.value == _value) return node;
+                node = node.next;
+            }
+            return null;
+        }
+
+        public List<Node> FindAll(int _value)
+        {
+            List<Node> nodes = new();
+            // здесь будет ваш код поиска всех узлов по заданному значению
+            Node node = head;
+            while (node != null)
+            {
+                if (node.value == _value) nodes.Add(node);
+                node = node.next;
+            }
+
+            return nodes;
+        }
+
+        public bool Remove(int _value)
+        {
+            // здесь будет ваш код удаления одного узла по заданному значению
+            Node node = head;
+            Node prevNode = null;
+            Node nextNode = node.next;
+
+            while (node != null)
+            {
+                if (node.value == _value)
+                {
+                    break;
+                }
+                prevNode = node;
+                node = node.next;
+            }
+
+
+            if (node == null)
+            {
+                return false;
+            }
+            else if (node == head)
+            {
+                head = nextNode;
+                return true;
+            }
+            else if (node == tail)
+            {
+                prevNode.next = null;
+                tail = prevNode;
+
+                return true;
+            }
+            else
+            {
+                prevNode.next = nextNode;
+                return true;
+            }
+        }
+
+        public void RemoveAll(int _value)
+        {
+            // здесь будет ваш код удаления всех узлов по заданному значению
+        }
+
+        public void Clear()
+        {
+            // здесь будет ваш код очистки всего списка
+            head = null;
+            tail = null;
+        }
+
+        public int Count()
+        {
+            // здесь будет ваш код подсчёта количества элементов в списке
+            Node node = head;
+            int count = 0;
+
+            while (node != null)
+            {
+                count++;
+                node = node.next;
+            }
+
+            return count;
+        }
+
+        public void InsertAfter(Node _nodeAfter, Node _nodeToInsert)
+        {
+            // здесь будет ваш код вставки узла после заданного
+
+            // если _nodeAfter = null , 
+            // добавьте новый элемент первым в списке 
+        }
+
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        LinkedList list = new();
+
+        Node node1 = new(1);
+        Node node2 = new(2);
+        Node node3 = new(2);
+        Node node4 = new(2);
+
+        list.AddInTail(node1);
+        list.AddInTail(node2);
+        list.AddInTail(node3);
+        list.AddInTail(node4);
+        list.AddInTail(new Node(3));
+
+        Node foundNode = list.Find(2);
+        List<Node> foundNodes = list.FindAll(2);
+
+        if (foundNode != null)
+        {
+            for (int i = 0; i < foundNodes.Count; i++)
+            {
+                Console.WriteLine("Found node at index " + i + " with value: " + foundNodes[i].value);
+            }
+        }
+        else
+        {
+            Console.WriteLine("No node with the specified value was found.");
+        }
+    }
+}
---
+++ TestTask1/UnitTest1.cs
@@ -0,0 +1,234 @@
+using AlgorithmsDataStructures;
+
+namespace TestTask1;
+
+public class UnitTest1
+{
+    [Fact]
+    public void CreatedNode_Test()
+    {
+        // Arrange
+        var list = new LinkedList();
+        var node = new Node(5);
+
+        // Act
+        list.AddInTail(node);
+
+        // Assert
+        Assert.Equal(node, list.tail);
+        Assert.Equal(node, list.head);
+    }
+
+    [Fact]
+    public void AddInTail_Test()
+    {
+        // Arrange
+        var list = new LinkedList();
+        var node = new Node(5);
+
+        // Act
+        list.AddInTail(node);
+
+        // Assert
+        Assert.Equal(node, list.tail);
+        Assert.Equal(node, list.head);
+    }
+
+    [Fact]
+    public void Find_Test()
+    {
+        var list = new LinkedList();
+        var node1 = new Node(1);
+        var node2 = new Node(2);
+        var node3 = new Node(3);
+        var node4 = new Node(4);
+
+        // Act
+        list.AddInTail(node1);
+        list.AddInTail(node2);
+        list.AddInTail(node3);
+        list.AddInTail(node4);
+
+        var findedNode1 = list.Find(2);
+        var findedNode2 = list.Find(3);
+
+        // Assert
+        Assert.Equal(findedNode1.value, 2);
+        Assert.Equal(findedNode2.value, 3);
+    }
+
+    [Fact]
+    public void Count_Test()
+    {
+        var list = new LinkedList();
+        var list2 = new LinkedList();
+        var node1 = new Node(1);
+        var node2 = new Node(2);
+        var node3 = new Node(3);
+        var node4 = new Node(4);
+
+        // Act
+        list.AddInTail(node1);
+        list.AddInTail(node2);
+
+        var lengthList1 = list.Count();
+
+        list.AddInTail(node3);
+        list.AddInTail(node4);
+
+        var lengthList2 = list.Count();
+        var lengthList3 = list2.Count();
+
+        // Assert
+        Assert.Equal(lengthList1, 2);
+        Assert.Equal(lengthList2, 4);
+        Assert.Equal(lengthList3, 0);
+    }
+
+    public bool AreEqual(LinkedList list1, LinkedList list2)
+    {
+        Node node1 = list1.head;
+        Node node2 = list2.head;
+
+        while (node1 != null || node2 != null)
+        {
+            if (node1 == null || node2 == null)
+            {
+                return false;
+            }
+
+            if (node1.value != node2.value)
+            {
+                return false;
+            }
+
+            node1 = node1.next;
+            node2 = node2.next;
+        }
+
+        return true;
+    }
+
+    [Fact]
+    public void Remove_Test_head()
+    {
+
+        var list1 = new LinkedList();
+        var list2 = new LinkedList();
+
+        var node1 = new Node(1);
+        var node2 = new Node(2);
+        var node3 = new Node(3);
+        var node4 = new Node(4);
+
+        var node12 = new Node(2);
+        var node13 = new Node(3);
+        var node14 = new Node(4);
+
+        // Act
+        list1.AddInTail(node1);
+        list1.AddInTail(node2);
+        list1.AddInTail(node3);
+        list1.AddInTail(node4);
+
+        list2.AddInTail(node12);
+        list2.AddInTail(node13);
+        list2.AddInTail(node14);
+
+        // Assert
+        bool result = list1.Remove(1);
+        Assert.True(result);
+        Assert.Null(list1.Find(1));
+        Assert.True(AreEqual(list1, list2));
+    }
+
+    [Fact]
+    public void Remove_Test_tail()
+    {
+        var list1 = new LinkedList();
+        var list2 = new LinkedList();
+
+        var node1 = new Node(1);
+        var node2 = new Node(2);
+        var node3 = new Node(3);
+        var node4 = new Node(4);
+
+        var node11 = new Node(1);
+        var node12 = new Node(2);
+        var node13 = new Node(3);
+
+        // Act
+        list1.AddInTail(node1);
+        list1.AddInTail(node2);
+        list1.AddInTail(node3);
+        list1.AddInTail(node4);
+
+        list2.AddInTail(node11);
+        list2.AddInTail(node12);
+        list2.AddInTail(node13);
+
+        // Assert
+        bool result = list1.Remove(4);
+        Assert.True(result);
+        Assert.Null(list1.Find(4));
+        Assert.True(AreEqual(list1, list2));
+    }
+
+    [Fact]
+    public void Remove_Test_general()
+    {
+        var list1 = new LinkedList();
+        var list2 = new LinkedList();
+
+        var node1 = new Node(1);
+        var node2 = new Node(2);
+        var node3 = new Node(3);
+        var node4 = new Node(4);
+
+        var node11 = new Node(1);
+        var node13 = new Node(3);
+        var node14 = new Node(4);
+
+        // Act
+        list1.AddInTail(node1);
+        list1.AddInTail(node2);
+        list1.AddInTail(node3);
+        list1.AddInTail(node4);
+
+        list2.AddInTail(node11);
+        list2.AddInTail(node13);
+        list2.AddInTail(node14);
+
+        // Assert
+        bool result = list1.Remove(2);
+        Assert.True(result);
+        Assert.Null(list1.Find(2));
+        Assert.True(AreEqual(list1, list2));
+    }
+
+    [Fact]
+    public void Clear_Test()
+    {
+        var list1 = new LinkedList();
+        var list2 = new LinkedList();
+
+        var node1 = new Node(1);
+        var node2 = new Node(2);
+        var node3 = new Node(3);
+        var node4 = new Node(4);
+
+        // Act
+        list1.AddInTail(node1);
+        list1.AddInTail(node2);
+        list1.AddInTail(node3);
+        list1.AddInTail(node4);
+
+        // Assert
+        list1.Clear();
+        Assert.Null(list1.Find(1));
+        Assert.Null(list1.Find(2));
+        Assert.Null(list1.Find(3));
+        Assert.Null(list1.Find(4));
+        Assert.True(AreEqual(list1, list2));
+    }
+}
