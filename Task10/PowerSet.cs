6264281aa131231d06b1db41590eb4ec98f24312
alex_di <mirag0505@yandex.ru>
2023-07-13 20:35:04 +0300

    Реализовал множество

--- Task10/PowerSet.cs
+++ Task10/PowerSet.cs
@@ -8,53 +8,76 @@ namespace AlgorithmsDataStructures
     // или расширьте его методами из HashTable
     public class PowerSet<T>
     {
-
+        private readonly List<T> elements;
         public PowerSet()
         {
             // ваша реализация хранилища
+            elements = new List<T>();
         }
 
         public int Size()
         {
             // количество элементов в множестве
-            return 0;
+            return elements.Count;
         }
 
         public void Put(T value)
         {
             // всегда срабатывает
+            if (!elements.Contains(value)) elements.Add(value);
         }
 
         public bool Get(T value)
         {
             // возвращает true если value имеется в множестве,
             // иначе false
-            return false;
+            return elements.Contains(value);
         }
 
         public bool Remove(T value)
         {
             // возвращает true если value удалено
             // иначе false
+            if (elements.Contains(value))
+            {
+                elements.Remove(value);
+                return true;
+            }
             return false;
         }
 
         public PowerSet<T> Intersection(PowerSet<T> set2)
         {
             // пересечение текущего множества и set2
-            return null;
+            PowerSet<T> set = new PowerSet<T>();
+
+            for (int i = 0; i < set2.elements.Count; i++)
+            {
+                if (elements.Contains(set2.elements[i])) set.Put(set2.elements[i]);
+            }
+            return set;
         }
 
         public PowerSet<T> Union(PowerSet<T> set2)
         {
             // объединение текущего множества и set2
-            return null;
+            for (int i = 0; i < elements.Count; i++)
+            {
+                set2.Put(elements[i]);
+            }
+            return set2;
         }
 
         public PowerSet<T> Difference(PowerSet<T> set2)
         {
             // разница текущего множества и set2
-            return null;
+            PowerSet<T> set = new PowerSet<T>();
+
+            for (int i = 0; i < set2.elements.Count; i++)
+            {
+                if (elements.Contains(set2.elements[i])) elements.Remove(set2.elements[i]);
+            }
+            return this;
         }
 
         public bool IsSubset(PowerSet<T> set2)
@@ -62,7 +85,11 @@ namespace AlgorithmsDataStructures
             // возвращает true, если set2 есть
             // подмножество текущего множества,
             // иначе false
-            return false;
+            for (int i = 0; i < set2.elements.Count; i++)
+            {
+                if (!elements.Contains(set2.elements[i])) return false;
+            }
+            return true;
         }
     }
 }
--- TestTask10/UnitTest1.cs
+++ TestTask10/UnitTest1.cs
@@ -1,10 +1,231 @@
 namespace TestTask10;
-
+using AlgorithmsDataStructures;
 public class UnitTest1
 {
     [Fact]
-    public void Test1()
+    public void CreatePowerSet()
     {
+        PowerSet<string> powerSet = new PowerSet<string>();
+        Assert.Equal(0, powerSet.Size());
+    }
 
+    [Fact]
+    public void PowerSet_Size()
+    {
+        PowerSet<string> powerSet = new PowerSet<string>();
+        Assert.Equal(0, powerSet.Size());
+        powerSet.Put("123");
+        Assert.Equal(1, powerSet.Size());
+    }
+
+    [Fact]
+    public void PowerSet_Put()
+    {
+        PowerSet<string> powerSet = new PowerSet<string>();
+        powerSet.Put("1");
+        powerSet.Put("1");
+        powerSet.Put("1");
+        Assert.Equal(1, powerSet.Size());
+        powerSet.Put("2");
+        Assert.Equal(2, powerSet.Size());
+
+    }
+
+    [Fact]
+    public void PowerSet_Remove()
+    {
+        PowerSet<string> powerSet = new PowerSet<string>();
+        powerSet.Put("1");
+        powerSet.Put("1");
+        powerSet.Put("1");
+        Assert.True(powerSet.Remove("1"));
+        Assert.Equal(0, powerSet.Size());
+
+        powerSet.Put("1");
+        powerSet.Put("2");
+        powerSet.Put("3");
+        powerSet.Put("1");
+        powerSet.Put("2");
+        powerSet.Put("3");
+        Assert.Equal(3, powerSet.Size());
+
+        Assert.True(powerSet.Remove("3"));
+        Assert.True(powerSet.Remove("2"));
+        Assert.True(powerSet.Remove("1"));
+        Assert.Equal(0, powerSet.Size());
+
+        Assert.False(powerSet.Remove("3"));
+        Assert.False(powerSet.Remove("2"));
+        Assert.False(powerSet.Remove("1"));
+        Assert.Equal(0, powerSet.Size());
+    }
+
+    [Fact]
+    public void PowerSet_Get()
+    {
+        PowerSet<string> powerSet = new PowerSet<string>();
+        powerSet.Put("1");
+        powerSet.Put("1");
+        powerSet.Put("1");
+        Assert.True(powerSet.Get("1"));
+        Assert.False(powerSet.Get("0"));
+
+        powerSet.Put("1");
+        powerSet.Put("2");
+        powerSet.Put("3");
+        powerSet.Put("1");
+        powerSet.Put("2");
+        powerSet.Put("3");
+        Assert.True(powerSet.Get("1"));
+        Assert.True(powerSet.Get("2"));
+        Assert.True(powerSet.Get("3"));
+        Assert.False(powerSet.Get("4"));
+    }
+
+    [Fact]
+    public void PowerSet_Without_Intersection()
+    {
+        PowerSet<string> powerSet1 = new PowerSet<string>();
+        PowerSet<string> powerSet2 = new PowerSet<string>();
+        powerSet1.Put("1");
+        powerSet1.Put("2");
+        powerSet1.Put("3");
+        powerSet2.Put("4");
+        powerSet2.Put("5");
+        powerSet2.Put("6");
+
+        var result = powerSet1.Intersection(powerSet2);
+        Assert.Equal(0, result.Size());
+    }
+
+    [Fact]
+    public void PowerSet_Intersection()
+    {
+        PowerSet<string> powerSet1 = new PowerSet<string>();
+        PowerSet<string> powerSet2 = new PowerSet<string>();
+        powerSet1.Put("1");
+        powerSet1.Put("2");
+        powerSet1.Put("3");
+        powerSet2.Put("4");
+        powerSet2.Put("2");
+        powerSet2.Put("3");
+
+        var result = powerSet1.Intersection(powerSet2);
+        Assert.Equal(2, result.Size());
+    }
+
+    [Fact]
+    public void PowerSet_Intersection_empty()
+    {
+        PowerSet<string> powerSet1 = new PowerSet<string>();
+        PowerSet<string> powerSet2 = new PowerSet<string>();
+
+        var result = powerSet1.Intersection(powerSet2);
+        Assert.Equal(0, result.Size());
+    }
+
+    [Fact]
+    public void PowerSet_Union_empty()
+    {
+        PowerSet<string> powerSet1 = new PowerSet<string>();
+        PowerSet<string> powerSet2 = new PowerSet<string>();
+
+        var result = powerSet1.Union(powerSet2);
+        Assert.Equal(0, result.Size());
+    }
+
+    [Fact]
+    public void PowerSet_Union()
+    {
+        PowerSet<string> powerSet1 = new PowerSet<string>();
+        PowerSet<string> powerSet2 = new PowerSet<string>();
+        powerSet1.Put("1");
+        powerSet1.Put("2");
+        powerSet1.Put("3");
+        powerSet2.Put("4");
+        powerSet2.Put("5");
+        powerSet2.Put("6");
+
+        var result = powerSet1.Union(powerSet2);
+        Assert.Equal(6, result.Size());
+    }
+
+    [Fact]
+    public void PowerSet_Union_with_similar_number()
+    {
+        PowerSet<string> powerSet1 = new PowerSet<string>();
+        PowerSet<string> powerSet2 = new PowerSet<string>();
+
+        powerSet1.Put("1");
+        powerSet1.Put("2");
+        powerSet1.Put("3");
+
+        powerSet2.Put("1");
+        powerSet2.Put("3");
+        powerSet2.Put("5");
+
+        var result = powerSet1.Union(powerSet2);
+        Assert.Equal(4, result.Size());
+    }
+
+    [Fact]
+    public void PowerSet_Difference_empty()
+    {
+        PowerSet<string> powerSet1 = new PowerSet<string>();
+        PowerSet<string> powerSet2 = new PowerSet<string>();
+
+        var result = powerSet1.Difference(powerSet2);
+        Assert.Equal(0, result.Size());
+    }
+
+    [Fact]
+    public void PowerSet_Difference()
+    {
+        PowerSet<string> powerSet1 = new PowerSet<string>();
+        PowerSet<string> powerSet2 = new PowerSet<string>();
+
+        powerSet1.Put("1");
+        powerSet1.Put("2");
+        powerSet1.Put("3");
+
+        powerSet2.Put("3");
+        powerSet2.Put("5");
+
+        var result = powerSet1.Difference(powerSet2);
+        Assert.Equal(2, result.Size());
+    }
+
+    [Fact]
+    public void PowerSet_IsSubset_true()
+    {
+        PowerSet<string> powerSet1 = new PowerSet<string>();
+        PowerSet<string> powerSet2 = new PowerSet<string>();
+
+        powerSet1.Put("1");
+        powerSet1.Put("2");
+        powerSet1.Put("3");
+
+        powerSet2.Put("1");
+        powerSet2.Put("3");
+
+        var result = powerSet1.IsSubset(powerSet2);
+        Assert.True(result);
+    }
+
+    [Fact]
+    public void PowerSet_IsSubset_false()
+    {
+        PowerSet<string> powerSet1 = new PowerSet<string>();
+        PowerSet<string> powerSet2 = new PowerSet<string>();
+
+        powerSet1.Put("1");
+        powerSet1.Put("2");
+        powerSet1.Put("3");
+
+        powerSet2.Put("3");
+        powerSet2.Put("6");
+
+        var result = powerSet1.IsSubset(powerSet2);
+        Assert.False(result);
     }
 }
