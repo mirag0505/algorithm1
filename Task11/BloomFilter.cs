297c87ca5c68f9943c2b224387f7e532d6bd3a3d
alex_di <mirag0505@yandex.ru>
2023-07-15 13:51:00 +0300

    добавил реализацию фильтра Блума

---
+++ Task11/BloomFilter.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections.Generic;
+using System;
+using System.IO;
+using System.Collections;
+
+namespace AlgorithmsDataStructures
+{
+    public class BloomFilter
+    {
+        public int filter_len;
+        public int bittest;
+
+        public BloomFilter(int f_len)
+        {
+            filter_len = f_len;
+            // создаём битовый массив длиной f_len ...
+            bittest = 0;
+        }
+
+        // хэш-функции
+        public int Hash1(string str1)
+        {
+            // 17
+            int hashIndex = 0;
+            int randomNumber = 17;
+
+            for (int i = 0; i < str1.Length; i++)
+            {
+                int code = (int)str1[i];
+                hashIndex = (hashIndex * randomNumber + code) % filter_len;
+            }
+
+            return hashIndex;
+            // "надо сделать из этого индекса битовую маску"
+        }
+        public int Hash2(string str1)
+        {
+            // 223
+            int hashIndex = 0;
+            int randomNumber = 223;
+
+            for (int i = 0; i < str1.Length; i++)
+            {
+                int code = (int)str1[i];
+                hashIndex = (hashIndex * randomNumber + code) % filter_len;
+            }
+            return hashIndex;
+        }
+
+        public void Add(string str1)
+        {
+            // добавляем строку str1 в фильтр
+            int index1 = Hash1(str1);
+            int index2 = Hash2(str1);
+
+            int mask1 = 0;
+            int mask2 = 0;
+            mask1 = mask1 | (1 << index1);
+            mask2 = mask2 | (1 << index2);
+
+            bittest = bittest | mask1;
+            bittest = bittest | mask2;
+        }
+
+        public bool IsValue(string str1)
+        {
+            // проверка, имеется ли строка str1 в фильтре
+            int index1 = Hash1(str1);
+            int index2 = Hash2(str1);
+
+            int mask1 = 1 << index1;
+            int mask2 = 1 << index2;
+
+            bool isInclude = (mask1 & bittest) != 0 && (mask2 & bittest) != 0;
+
+            return isInclude;
+        }
+    }
+}
--- TestTask11/UnitTest1.cs
+++ TestTask11/UnitTest1.cs
@@ -1,10 +1,49 @@
 namespace TestTask11;
-
+using AlgorithmsDataStructures;
 public class UnitTest1
 {
     [Fact]
-    public void Test1()
+    public void CreateBloomFilter()
     {
+        BloomFilter bloomFilter = new BloomFilter(32);
+        Assert.Equal(32, bloomFilter.filter_len);
+    }
 
+    [Fact]
+    public void BloomFilter_Hash1()
+    {
+        BloomFilter bloomFilter = new BloomFilter(32);
+        var result = bloomFilter.Hash1("0123456789");
+        Assert.Equal(7, result);
+        Assert.Equal("111", Convert.ToString(result, 2));
+    }
+
+    [Fact]
+    public void BloomFilter_Hash2()
+    {
+        BloomFilter bloomFilter = new BloomFilter(32);
+        var result = bloomFilter.Hash2("0123456789");
+        Assert.Equal(9, result);
+    }
+
+    [Fact]
+    public void BloomFilter_Add()
+    {
+        BloomFilter bloomFilter = new BloomFilter(32);
+        bloomFilter.Add("0123456789");
+        Assert.Equal("1010000000", Convert.ToString(bloomFilter.bittest, 2));
+    }
+
+    [Fact]
+    public void BloomFilter_IsValue()
+    {
+        BloomFilter bloomFilter = new BloomFilter(32);
+        bloomFilter.Add("0123456789");
+
+        Assert.True(bloomFilter.IsValue("0123456789"));
+        Assert.False(bloomFilter.IsValue("1234567890"));
+        Assert.False(bloomFilter.IsValue("8901234567"));
+        Assert.False(bloomFilter.IsValue("9012345678"));
+        Assert.False(bloomFilter.IsValue("3645"));
     }
 }
